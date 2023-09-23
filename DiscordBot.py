cd7ca3ddc7e45f3d5c1f099c5b55618f97a4ca6c
KTN44295080 <koutyan44295080@icloud.com>
2023-09-23 21:45:08 +0900

    Initial commit

---
+++ DiscordBot.py
@@ -0,0 +1,215 @@
+import re
+import random
+import unicodedata
+import mojimoji
+import discord
+from discord.ext import commands
+from discord.commands import Option
+
+class GameStats :
+    def __init__(self) :
+        self.division = 3
+        self.arrangeCount = self.division
+        self.fields = [[0] * self.division for i in range(self.division)]
+        self.squareCount = self.division * self.division
+        self.turn = 0
+        self.isMatch = False
+        self.playerNames = []
+        self.playerIds = []
+        self.turnPlayer = 0
+
+    def Reset(self) :
+        self.fields = [[0] * self.division for i in range(self.division)]
+        self.isMatch = False
+        self.turn = 0
+        self.squareCount = self.division * self.division
+        self.playerNames = []
+        self.playerIds = []
+        self.turnPlayer = 0
+
+
+TOKEN = "WRITE YOUR TOKEN"
+status = "/play"
+statustype = discord.ActivityType.watching
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+gameStats = GameStats()
+
+@bot.event
+async def on_ready():
+    print("Ready")
+    await bot.change_presence(activity = discord.Activity(name=status, type=statustype))
+
+
+@bot.event
+async def on_message(message):
+    if not gameStats.isMatch or not message.author.id == gameStats.playerIds[gameStats.turnPlayer]:
+        return
+    else:
+        result = int(re.sub(r"\D", "", unicodedata.normalize("NFKC", message.content)))
+        if result > 0 and result <= gameStats.squareCount:
+            index = divmod(result - 1, gameStats.division)
+            if gameStats.fields[index[0]][index[1]] != 0:
+                await message.channel.send("そのマスには既に置かれています")
+                return
+            gameStats.fields[index[0]][index[1]] = gameStats.turnPlayer + 1
+            await message.channel.send(DrawField())
+            gameStats.turn += 1
+            if LineCheck() != 0:
+                await message.channel.send(f"{gameStats.playerNames[gameStats.turnPlayer]}さんの勝利です！")
+                gameStats.Reset()
+                return
+            if gameStats.turn == gameStats.squareCount:
+                await message.channel.send("引き分けです！")
+                gameStats.Reset()
+                return
+            if len(gameStats.playerNames) < 2 :
+                await message.channel.send(f"コンピューターのターンです")
+                pos = ReachCheck()
+                if pos == None:
+                    while True:
+                        x = random.randint(0, gameStats.division - 1)
+                        y = random.randint(0, gameStats.division - 1)
+                        if gameStats.fields[y][x] == 0:
+                            pos = y, x
+                            break
+                gameStats.fields[pos[0]][pos[1]] = 2
+                await message.channel.send(DrawField())
+                gameStats.turn += 1
+                if LineCheck() != 0:
+                    await message.channel.send("コンピューターの勝利です！")
+                    gameStats.Reset()
+                    return
+                if gameStats.turn == gameStats.squareCount:
+                    await message.channel.send("引き分けです！")
+                    gameStats.Reset()
+                    return
+                await message.channel.send(f"{gameStats.playerNames[0]}さんのターンです")
+            else :
+                gameStats.turnPlayer += 1
+                if gameStats.turnPlayer >= len(gameStats.playerNames) :
+                    gameStats.turnPlayer = 0
+                await message.channel.send(f"{gameStats.playerNames[gameStats.turnPlayer]}さんのターンです")
+
+async def get_players(ctx) :
+    return [user.display_name for user in bot.get_channel(ctx.interaction.channel_id).members if user.display_name.startswith(ctx.value) and not user.bot]
+
+
+@bot.slash_command(description = "三目並べを開始します")
+async def play(ctx, player: Option(str, "対戦相手を指定してください。指定しない場合、コンピューターが相手になります。", autocomplete = get_players) = None, square : Option(int, "何マス×何マスでプレイするか指定してください。指定しない場合、3×3になります。") = 3) :
+    if square <= 0 :
+        await force_end(ctx, square)
+        return
+    playerInUsers = False
+    for user in bot.get_channel(ctx.interaction.channel_id).members :
+        if player == user.display_name :
+            playerInUsers = True
+            break
+    if not playerInUsers :
+        await ctx.respond(f"{player}という名前のユーザーが存在しません")
+        return
+    if player == ctx.author.display_name :
+        await ctx.respond("【注意】己との対戦になります")
+    gameStats.division = square
+    gameStats.arrangeCount = square
+    gameStats.Reset()
+    gameStats.isMatch = True
+    gameStats.playerNames.append(ctx.author.display_name)
+    gameStats.playerIds.append(ctx.author.id)
+    if(not player == None) :
+        gameStats.playerIds.append([user.id for user in bot.users if user.display_name == player][0])
+        gameStats.playerNames.append(player)
+
+    await ctx.respond(f"{square}×{square}マスで始めます。\n{ctx.author.display_name}さん、置く場所を1~{gameStats.squareCount}で指定してください")
+    textField = ""
+    loopCount = 0
+    for i in range(len(gameStats.fields)) :
+        for j in range(len(gameStats.fields[i])):
+            if loopCount % gameStats.division == 0:
+                textField += "\n"
+            loopCount += 1
+            if loopCount < 10 :
+                textField += mojimoji.han_to_zen(f"{loopCount}")
+            else :
+                textField += "／"
+    await ctx.send(textField)
+
+
+@bot.slash_command(description = f"{gameStats.division}目並べを終了します")
+async def end(ctx):
+    await ctx.respond(f"{gameStats.division}目並べを終了しました")
+    gameStats.Reset()
+
+
+async def force_end(ctx, square):
+    if (not square == 0):
+        await ctx.respond(f"{square}目並べに失敗しました。正の値目並べを開始してください。")
+    else:
+        await ctx.respond(f"{square}x{square}マスで始めます。おはよう{ctx.author.display_name} お前の勝ちだよ")
+    return
+
+
+def DrawField():
+    textField = ""
+    loopCount = 0
+    for i in range(len(gameStats.fields)):
+        for j in range(len(gameStats.fields[i])):
+            if loopCount % gameStats.division == 0:
+                textField += "\n"
+            loopCount += 1
+
+            if gameStats.fields[i][j] == 1:
+                textField += "〇"
+            elif gameStats.fields[i][j] == 2:
+                textField += "✕"
+            else:
+                if loopCount < 10 :
+                    textField += mojimoji.han_to_zen(f"{loopCount}")
+                else :
+                    textField += mojimoji.han_to_zen(f"／")
+    return textField
+
+
+def ReachCheck():
+    for i in reversed(range(1, 3)):
+        for y in range(gameStats.division):
+            if gameStats.fields[y].count(i) == gameStats.arrangeCount - 1 and 0 in gameStats.fields[y]:
+                if i != 0:
+                    return y, gameStats.fields[y].index(0)
+
+        for x in range(gameStats.division):
+            column = [c[x] for c in gameStats.fields]
+            if column.count(i) == gameStats.arrangeCount - 1 and 0 in column:
+                if i != 0:
+                    return column.index(0), x
+
+        diagonal = [gameStats.fields[n][n] for n in range(gameStats.division)]
+        if diagonal.count(i) == gameStats.arrangeCount - 1 and 0 in diagonal:
+            index = diagonal.index(0)
+            return index, index
+        diagonal = [gameStats.fields[n][gameStats.division - 1 - n] for n in range(gameStats.division)]
+        if diagonal.count(i) == gameStats.arrangeCount - 1 and 0 in diagonal:
+            index = diagonal.index(0)
+            return index, gameStats.division - 1 - index
+
+
+def LineCheck():
+    winner = 0
+    for i in range(1, 3):
+        for x in range(gameStats.division):
+            column = [c[x] for c in gameStats.fields]
+            if column.count(i) == gameStats.arrangeCount:
+                winner = i
+        for y in range(gameStats.division):
+            if gameStats.fields[y].count(i) == gameStats.arrangeCount:
+                winner = i
+
+        diagonal = [gameStats.fields[n][n] for n in range(gameStats.division)]
+        if diagonal.count(i) == gameStats.arrangeCount:
+            winner = i
+        diagonal = [gameStats.fields[n][gameStats.division - 1 - n] for n in range(gameStats.division)]
+        if diagonal.count(i) == gameStats.arrangeCount:
+            winner = i
+    return winner
+
+
+bot.run(TOKEN)
---
+++ README.md
@@ -0,0 +1,38 @@
+# n目並べDiscordBot
+Discord内で動作するn目並べで遊べるBot。<br>
+コンピューターとの対戦と2人プレイの両方に対応。
+
+# Requirement
+
+### 環境
+* Pythom 3.11
+### 必要なパッケージ
+* mojimoji
+* discord.py
+* py-cord
+
+# Installation
+以下のサイトでDiscordのBotを作成し、アクセストークンの取得と権限の付与を行う。<br>
+https://discord.com/developers/applications?new_application=true
+### アクセストークンの取得
+New Applicationを押下、適当な名前を入力しCreate。<br>
+左側のメニューからBotを選択し、Reset Tokenボタンを押下するとアクセストークンが表示されるのでCopyボタンを押下してコピーする。<br>
+コピーしたトークンをDiscordBot.py内31行目のWRITE YOUR TOKENを上書きする形でペースト。
+### 権限の付与
+左側メニューからBotを選択し、Privileged Gateway Intents内のPRESENCE INTENT、SERVER MEMBERS INTENT、MESSAGE CONTENT INTENTを有効化し、下部に表示されるSave Changesボタンを押下して保存する。<br>
+次に左側のメニューからOAuth2→URL Generatorを選択し、SCOPES内のbotとapllications.commandsにチェックを入れた後。下に表示されるBOT PERMISSIONS内のSend Messagessにもチェックを入れる。<br>
+最後にその下に表示されるURLをCopyしてアクセスすればBotを任意のサーバーに追加できる。
+### 動作に必要なパッケージをインポート
+ターミナル等でCloneした本リポジトリのフォルダに移動し、以下のコマンドを実行する。
+
+```bash
+pip install -r requirements.txt
+```
+
+# Usage
+
+パッケージをインストール後、ターミナル等でCloneした本リポジトリのフォルダに移動し、以下のコマンドを実行する。
+
+```bash
+python DiscordBot.py
+```
