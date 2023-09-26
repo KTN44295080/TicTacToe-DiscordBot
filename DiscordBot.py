e8cf6d1899d5523e5cdba2af111c44f28bb1f80c
KTN44295080 <47689501+KTN44295080@users.noreply.github.com>
2023-09-26 16:25:47 +0900

    不具合の修正
    ユーザー名を指定せずに/playコマンドを使用した際、コンピューターとの対戦が始まらずエラーが出てしまう不具合を修正しました。
    Noneというニックネームのユーザーを対戦相手に指定した際、対戦相手をコンピューターと誤認する問題を修正しました。

--- DiscordBot.py
+++ DiscordBot.py
@@ -6,8 +6,9 @@ import discord
 from discord.ext import commands
 from discord.commands import Option
 
-class GameStats :
-    def __init__(self) :
+
+class GameStats:
+    def __init__(self):
         self.division = 3
         self.arrangeCount = self.division
         self.fields = [[0] * self.division for i in range(self.division)]
@@ -18,7 +19,7 @@ class GameStats :
         self.playerIds = []
         self.turnPlayer = 0
 
-    def Reset(self) :
+    def Reset(self):
         self.fields = [[0] * self.division for i in range(self.division)]
         self.isMatch = False
         self.turn = 0
@@ -34,15 +35,19 @@ statustype = discord.ActivityType.watching
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 gameStats = GameStats()
 
+
 @bot.event
 async def on_ready():
     print("Ready")
-    await bot.change_presence(activity = discord.Activity(name=status, type=statustype))
+    await bot.change_presence(activity=discord.Activity(name=status, type=statustype))
 
 
 @bot.event
 async def on_message(message):
-    if not gameStats.isMatch or not message.author.id == gameStats.playerIds[gameStats.turnPlayer]:
+    if (
+        not gameStats.isMatch
+        or not message.author.id == gameStats.playerIds[gameStats.turnPlayer]
+    ):
         return
     else:
         result = int(re.sub(r"\D", "", unicodedata.normalize("NFKC", message.content)))
@@ -55,14 +60,16 @@ async def on_message(message):
             await message.channel.send(DrawField())
             gameStats.turn += 1
             if LineCheck() != 0:
-                await message.channel.send(f"{gameStats.playerNames[gameStats.turnPlayer]}さんの勝利です！")
+                await message.channel.send(
+                    f"{gameStats.playerNames[gameStats.turnPlayer]}さんの勝利です！"
+                )
                 gameStats.Reset()
                 return
             if gameStats.turn == gameStats.squareCount:
                 await message.channel.send("引き分けです！")
                 gameStats.Reset()
                 return
-            if len(gameStats.playerNames) < 2 :
+            if len(gameStats.playerNames) < 2:
                 await message.channel.send(f"コンピューターのターンです")
                 pos = ReachCheck()
                 if pos == None:
@@ -84,67 +91,87 @@ async def on_message(message):
                     gameStats.Reset()
                     return
                 await message.channel.send(f"{gameStats.playerNames[0]}さんのターンです")
-            else :
+            else:
                 gameStats.turnPlayer += 1
-                if gameStats.turnPlayer >= len(gameStats.playerNames) :
+                if gameStats.turnPlayer >= len(gameStats.playerNames):
                     gameStats.turnPlayer = 0
-                await message.channel.send(f"{gameStats.playerNames[gameStats.turnPlayer]}さんのターンです")
+                await message.channel.send(
+                    f"{gameStats.playerNames[gameStats.turnPlayer]}さんのターンです"
+                )
 
-async def get_players(ctx) :
-    return [user.display_name for user in bot.get_channel(ctx.interaction.channel_id).members if user.display_name.startswith(ctx.value) and not user.bot]
+
+async def get_players(ctx):
+    return [
+        user.display_name
+        for user in bot.get_channel(ctx.interaction.channel_id).members
+        if user.display_name.startswith(ctx.value) and not user.bot
+    ]
 
 
-@bot.slash_command(description = "三目並べを開始します")
-async def play(ctx, player: Option(str, "対戦相手を指定してください。指定しない場合、コンピューターが相手になります。", autocomplete = get_players) = None, square : Option(int, "何マス×何マスでプレイするか指定してください。指定しない場合、3×3になります。") = 3) :
-    if square <= 0 :
+@bot.slash_command(description="三目並べを開始します")
+async def play(
+    ctx,
+    player: Option(
+        str, "対戦相手を指定してください。指定しない場合、コンピューターが相手になります。", autocomplete=get_players
+    ) = "",
+    square: Option(int, "何マス×何マスでプレイするか指定してください。指定しない場合、3×3になります。") = 3,
+):
+    if square <= 0:
         await force_end(ctx, square)
         return
-    playerInUsers = False
-    for user in bot.get_channel(ctx.interaction.channel_id).members :
-        if player == user.display_name :
-            playerInUsers = True
-            break
-    if not playerInUsers :
-        await ctx.respond(f"{player}という名前のユーザーが存在しません")
-        return
-    if player == ctx.author.display_name :
-        await ctx.respond("【注意】己との対戦になります")
+    if not player == "":
+        playerInUsers = False
+        for user in bot.get_channel(ctx.interaction.channel_id).members:
+            if player == user.display_name:
+                playerInUsers = True
+                break
+        if not playerInUsers:
+            await ctx.respond(f"{player}という名前のユーザーが存在しません")
+            return
+        if player == ctx.author.display_name:
+            await ctx.respond("【注意】己との対戦になります")
     gameStats.division = square
     gameStats.arrangeCount = square
     gameStats.Reset()
     gameStats.isMatch = True
     gameStats.playerNames.append(ctx.author.display_name)
     gameStats.playerIds.append(ctx.author.id)
-    if(not player == None) :
-        gameStats.playerIds.append([user.id for user in bot.users if user.display_name == player][0])
+    if not player == "":
+        gameStats.playerIds.append(
+            [user.id for user in bot.users if user.display_name == player][0]
+        )
         gameStats.playerNames.append(player)
 
-    await ctx.respond(f"{square}×{square}マスで始めます。\n{ctx.author.display_name}さん、置く場所を1~{gameStats.squareCount}で指定してください")
+    await ctx.respond(
+        f"{square}×{square}マスで始めます。\n{ctx.author.display_name}さん、置く場所を1~{gameStats.squareCount}で指定してください"
+    )
     textField = ""
     loopCount = 0
-    for i in range(len(gameStats.fields)) :
+    for i in range(len(gameStats.fields)):
         for j in range(len(gameStats.fields[i])):
             if loopCount % gameStats.division == 0:
                 textField += "\n"
             loopCount += 1
-            if loopCount < 10 :
+            if loopCount < 10:
                 textField += mojimoji.han_to_zen(f"{loopCount}")
-            else :
+            else:
                 textField += "／"
     await ctx.send(textField)
 
 
-@bot.slash_command(description = f"{gameStats.division}目並べを終了します")
+@bot.slash_command(description=f"{gameStats.division}目並べを終了します")
 async def end(ctx):
     await ctx.respond(f"{gameStats.division}目並べを終了しました")
     gameStats.Reset()
 
 
 async def force_end(ctx, square):
-    if (not square == 0):
+    if not square == 0:
         await ctx.respond(f"{square}目並べに失敗しました。正の値目並べを開始してください。")
     else:
-        await ctx.respond(f"{square}x{square}マスで始めます。おはよう{ctx.author.display_name} お前の勝ちだよ")
+        await ctx.respond(
+            f"{square}x{square}マスで始めます。おはよう{ctx.author.display_name} お前の勝ちだよ"
+        )
     return
 
 
@@ -162,9 +189,9 @@ def DrawField():
             elif gameStats.fields[i][j] == 2:
                 textField += "✕"
             else:
-                if loopCount < 10 :
+                if loopCount < 10:
                     textField += mojimoji.han_to_zen(f"{loopCount}")
-                else :
+                else:
                     textField += mojimoji.han_to_zen(f"／")
     return textField
 
@@ -172,7 +199,10 @@ def DrawField():
 def ReachCheck():
     for i in reversed(range(1, 3)):
         for y in range(gameStats.division):
-            if gameStats.fields[y].count(i) == gameStats.arrangeCount - 1 and 0 in gameStats.fields[y]:
+            if (
+                gameStats.fields[y].count(i) == gameStats.arrangeCount - 1
+                and 0 in gameStats.fields[y]
+            ):
                 if i != 0:
                     return y, gameStats.fields[y].index(0)
 
@@ -186,7 +216,10 @@ def ReachCheck():
         if diagonal.count(i) == gameStats.arrangeCount - 1 and 0 in diagonal:
             index = diagonal.index(0)
             return index, index
-        diagonal = [gameStats.fields[n][gameStats.division - 1 - n] for n in range(gameStats.division)]
+        diagonal = [
+            gameStats.fields[n][gameStats.division - 1 - n]
+            for n in range(gameStats.division)
+        ]
         if diagonal.count(i) == gameStats.arrangeCount - 1 and 0 in diagonal:
             index = diagonal.index(0)
             return index, gameStats.division - 1 - index
@@ -206,7 +239,10 @@ def LineCheck():
         diagonal = [gameStats.fields[n][n] for n in range(gameStats.division)]
         if diagonal.count(i) == gameStats.arrangeCount:
             winner = i
-        diagonal = [gameStats.fields[n][gameStats.division - 1 - n] for n in range(gameStats.division)]
+        diagonal = [
+            gameStats.fields[n][gameStats.division - 1 - n]
+            for n in range(gameStats.division)
+        ]
         if diagonal.count(i) == gameStats.arrangeCount:
             winner = i
     return winner
